264db259395c760a06749b7d47224e7846332d2d
Johann <dbgljk.ff@gmail.com>
2023-02-27 22:56:56 +0300

    added Finder implementation

--- src/Observer.server/Server.Core/Services/Finder.cs
+++ src/Observer.server/Server.Core/Services/Finder.cs
@@ -1,3 +1,7 @@
+using HtmlAgilityPack;
+
+using Microsoft.Extensions.Logging;
+
 using Server.Abstractions.Parser;
 using Server.Models.Requests;
 using Server.Models.Responses;
@@ -6,8 +10,37 @@ namespace Server.Core.Services;
 
 public sealed class Finder : IFinder
 {
+    private readonly ILogger<Finder> _logger;
+
+    public Finder(ILogger<Finder> logger)
+    {
+        _logger = logger;
+    }
+
     public async Task<Response> FindValueAsync(FindValueRequest request)
     {
-        return await Task.FromResult(new Response(Models.Enums.ResponseCode.SUCCESS, "ok", Guid.NewGuid()));
+        _logger.LogInformation($"requested url: {request.LocatorUrl}, xPath: {request.Locator}");
+
+        string value = await GetValueFromUrlAsync(request.LocatorUrl, request.Locator) ?? "Could not find the value";
+
+        return new Response(Models.Enums.ResponseCode.SUCCESS, value, Guid.NewGuid());
+    }
+
+    private async Task<string?> GetValueFromUrlAsync(string url, string xPath)
+    {
+        HtmlNode htmlNode = await LoadHtmlNodeAsync(url);
+        HtmlNode? foundedValue = htmlNode.SelectSingleNode(xPath);
+
+        return foundedValue?.InnerText;
+    }
+
+    private async Task<HtmlNode> LoadHtmlNodeAsync(string url)
+    {
+        var web = new HtmlWeb();
+        HtmlDocument htmlDoc = await web.LoadFromWebAsync(url);
+
+        _logger.LogDebug($"html: {htmlDoc.DocumentNode.InnerHtml}, response status: {web.StatusCode}");
+
+        return htmlDoc.DocumentNode;
     }
 }
